cc625055381a6ea85a1075d9b1fbe06dcd88d32c
Lutfur Rahman Jamil <93875782+JamilLabLTD@users.noreply.github.com>
2022-03-03 22:57:45 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,24 @@
+/*Nav Bar Animation*/
+function ThreeDothide(){
+  x =  document.getElementById("h_o_left_area");
+  x.style.marginLeft = "0";
+  document.getElementById("h_o_right_area").style.left = "20%";
+}
+
+
+function darkLeftOnclick(){
+  x =  document.getElementById("h_o_left_area");
+  x.style.marginLeft = "-20%";
+  document.getElementById("h_o_right_area").style.left = "100%";
+}
+function OcloseLeft(){
+  x =  document.getElementById("h_o_left_area");
+  x.style.marginLeft = "-20%";
+  document.getElementById("h_o_right_area").style.left = "100%";
+}
+function OcloseRight(){
+  x =  document.getElementById("h_o_left_area");
+  x.style.marginLeft = "-20%";
+  document.getElementById("h_o_right_area").style.left = "100%";
+}
+/*--------***----------*/
